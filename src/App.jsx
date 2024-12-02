dec3203161713c982fa6f366ab17ea7128274f77
waitletmecook <zzula5152@gmail.com>
2024-12-02 15:31:40 +0300

    Гончаров

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App'
+import './index.css';
+
+function App() {
+  return (
+    <div className='Banner'>
+      <div className='Banner w-[1920px] h-11 px-4 py-3 bg-[#FC7A1D] flex-col justify-start gap-2 inline-flex text-center'>
+        <p>Summer sale up to 50% off</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
